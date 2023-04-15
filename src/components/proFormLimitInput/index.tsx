6c821bc211555051c881f472ab25994d37cedba4
loclink <loclink@163.com>
2023-04-15 19:51:16 +0800

    fix: 更改limitInput字体颜色，去除冲突依赖

--- src/components/proFormLimitInput/index.tsx
+++ src/components/proFormLimitInput/index.tsx
@@ -1,31 +1,31 @@
-import { ProFormField } from '@ant-design/pro-form';
-import { concatRule } from '../../form-rules';
-import { Input, InputProps } from 'antd';
-import { FC, memo, useMemo } from 'react';
+import { ProFormField } from '@ant-design/pro-form'
+import { concatRule } from '../../form-rules'
+import { Input, InputProps } from 'antd'
+import { FC, memo, useMemo } from 'react'
 // import styles from './index.module.less'
-import { ProFormItemProps } from '@ant-design/pro-form';
+import { ProFormItemProps } from '@ant-design/pro-form'
 
 export interface IProFormLimitInputProps extends ProFormItemProps<InputProps> {
   /** 最大长度 */
-  maxLength?: number;
+  maxLength?: number
 }
 
-const Component: FC<IProFormLimitInputProps> = props => {
-  const { fieldProps, maxLength = 12, disabled, rules, ...formProps } = props;
+const Component: FC<IProFormLimitInputProps> = (props) => {
+  const { fieldProps, maxLength = 12, disabled, rules, ...formProps } = props
 
-  const innerRules = useMemo(() => concatRule([...(rules ?? []), 'trim']), [rules]);
+  const innerRules = useMemo(() => concatRule([...(rules ?? []), 'trim']), [rules])
 
   return (
     <ProFormField {...formProps} rules={innerRules}>
       <LimitInput {...fieldProps} maxLength={maxLength} disabled={disabled} />
     </ProFormField>
-  );
-};
+  )
+}
 
-Component.displayName = 'ProFormLimitInput';
+Component.displayName = 'ProFormLimitInput'
 
-const ProFormLimitInput = memo(Component);
-export default ProFormLimitInput;
+const ProFormLimitInput = memo(Component)
+export default ProFormLimitInput
 
 const LimitInput = memo(({ value, maxLength, onChange, ...fieldProps }: any) => {
   return (
@@ -35,10 +35,10 @@ const LimitInput = memo(({ value, maxLength, onChange, ...fieldProps }: any) => 
       value={value}
       onChange={onChange}
       suffix={
-        <span style={{ color: '#333' }}>
+        <span style={{ color: '#ccc' }}>
           {((value || '') as string).length}/{maxLength}
         </span>
       }
     />
-  );
-});
+  )
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import EnhanceTable from './components/EnhanceTable';
-
-export { EnhanceTable };
+import EnhanceTable from './components/EnhanceTable'
+import WmTableSwitch from './components/TableSwitch'
+export * from './hooks/types'
+export { EnhanceTable, WmTableSwitch }
